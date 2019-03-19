20d882cdb71fc12113e1136213cff8045fb9ad64
PiranhaGod420 <f.hanschristian@gmail.com>
2019-03-19 09:28:49 +0100

    cleaned up structure; working build; small adjustments

--- CsvGenerator.py
+++ CsvGenerator.py
@@ -1,49 +1,56 @@
 import pandas as pd
+from os import mkdir
+from os.path import isdir, join
 
 import config
 
 
 class CsvGenerator:
-    constants = config.Constants()
-    root_directory = constants.DIR_TO_LOOK_IN
-    files = constants.FILE_LIST
-    columns = constants.COLUMN_LIST
 
-    def __init__(
-            self,
-            start_date,
-            num_of_days,
-    ):
+    def __init__(self, start_date, num_of_days,):
         self.start_date = start_date
         self.num_of_days = num_of_days
 
+        self.const = config.Constants()
+
+        self.__prepare_environment()
+
+    def __prepare_environment(self):
+        if not isdir(self.const.DIR_GENERATED_FILES):
+            mkdir(self.const.DIR_GENERATED_FILES)
+
     def generate_csv_for_machine(self, machine_nr):
-        for file in self.files:
+        for file in self.const.FILE_LIST:
             curr_file = pd.read_csv(
-                self.root_directory + file,
+                join(self.const.PATH_ROOT, file),
                 sep='|',
                 low_memory=False,
-                #skiprows=
-                #nrows=
+                # skiprows=
+                # nrows=
             )
 
     def generate_csv_from_columns(self, rows_to_skip, rows_to_read):
-        for file in self.files:
+        for file in self.const.FILE_LIST:
             curr_file = pd.read_csv(
-                self.root_directory + file,
+                join(self.const.PATH_ROOT, file),
                 sep='|',
                 low_memory=False,
                 skiprows=rows_to_skip,
                 nrows=rows_to_read
             )
 
-            data_frame = curr_file[[col for col in self.columns]]
+            data_frame = curr_file[[col for col in self.const.COLUMN_LIST]]
 
+            '''
             for col in data_frame.columns:
                 if "$COLUMNS$" in col:
                     data_frame.rename(columns={col: col.split("$COLUMNS$")[-1]}, inplace=True)
                     self.columns[self.columns.index(col)] = col.split("$COLUMNS$")[-1]
+            '''
 
-            generated_filename = file + "_cols_" + '-'.join(col for col in self.columns) + ".csv"
+            generated_filename = '{}_cols_{}.csv'.format(
+                join(self.const.DIR_GENERATED_FILES, file),
+                '-'.join(col for col in self.const.COLUMN_LIST)
+            )
             with open(generated_filename, "w+") as output:
                 output.write(pd.DataFrame.to_csv(data_frame))
--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from os.path import isfile, join, realpath
 
 class Constants:
     def __init__(self):
-        self.__path_list = realpath(__file__).split("\\")
-        self.ROOT_PATH = '\\'.join(self.__path_list[:len(self.__path_list) - 1], )
+        self.__PATH_LIST = realpath(__file__).split("\\")
+        self.PATH_ROOT = '\\'.join(self.__PATH_LIST[:len(self.__PATH_LIST) - 1], )
 
-        self.DIR_TO_LOOK_IN = self.ROOT_PATH + "\\test_events\\"
-        self.DIR_TO_SAVE_IN = self.ROOT_PATH + "\\generated_csvs\\"
-        self.FILE_LIST = [file for file in listdir(self.DIR_TO_LOOK_IN) if isfile(join(self.DIR_TO_LOOK_IN, file))]
+        self.DIR_BASE_FILES = join(self.PATH_ROOT, "test_events")
+        self.DIR_GENERATED_FILES = join(self.PATH_ROOT, "generated_csvs")
+        self.FILE_LIST = [file for file in listdir(self.DIR_BASE_FILES) if isfile(join(self.DIR_BASE_FILES, file))]
 
         self.COLUMN_LIST = [
             "$COLUMNS$MASCH_NR",
