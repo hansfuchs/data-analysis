aa4a11f9fdea193847d59e786ba7a3f1c66118e6
hansfuchs <f.hanschristian@gmail.com>
2019-04-29 20:41:06 +0200

    small adjustments

--- DataGapClassifier.py
+++ DataGapClassifier.py
@@ -54,55 +54,53 @@ class DataGapClassifier:
             for date in self.dates:     # type: str
                 if date not in df[self.const.COL_DATE].unique():
                     missing_days.append(date)
-            print(missing_days)
 
             for day in missing_days:    # type: str
                 # check whether day is inside an already documented time range and continue if True
-                if data_dict:
-                    machine_nrs: Set[str] = set(data_dict['MACHINE_NR'])
-                    already_checked_days: Dict[str, List[str]] = {}
-                    for machine in machine_nrs:
-                        already_checked_days[machine] = []
+                machine_nrs: Set[str] = set(data_dict['MACHINE_NR'])
+                already_checked_days: Dict[str, List[str]] = {}
+                for machine in machine_nrs:
+                    already_checked_days[machine] = []
 
-                    for index in range(0, len(data_dict['BEGIN_DATE'])):
-                        curr_machine: str = data_dict['MACHINE_NR'][index]
-                        curr_day: datetime.datetime = string_to_date2(data_dict['BEGIN_DATE'][index])
-                        end_day: datetime.datetime = string_to_date2(data_dict['END_DATE'][index])
+                for index in range(0, len(data_dict['BEGIN_DATE'])):
+                    curr_machine: str = data_dict['MACHINE_NR'][index]
+                    curr_day: datetime.datetime = string_to_date2(data_dict['BEGIN_DATE'][index])
+                    end_day: datetime.datetime = string_to_date2(data_dict['END_DATE'][index])
 
+                    already_checked_days[curr_machine].append(curr_day.strftime('%Y-%m-%d'))
+                    while curr_day != end_day:
+                        curr_day += datetime.timedelta(days=1)
                         already_checked_days[curr_machine].append(curr_day.strftime('%Y-%m-%d'))
-                        while curr_day != end_day:
-                            curr_day += datetime.timedelta(days=1)
-                            already_checked_days[curr_machine].append(curr_day.strftime('%Y-%m-%d'))
 
-                    day_already_checked: bool = False
-                    for machine, days in already_checked_days.items():
-                        if machine == df[self.const.COL_MACHINE_NR].values[0] and day in already_checked_days[machine]:
-                            day_already_checked = True
+                day_already_checked: bool = False
+                for machine, days in already_checked_days.items():
+                    if machine == df[self.const.COL_MACHINE_NR].values[0] and day in already_checked_days[machine]:
+                        day_already_checked = True
 
-                    if day_already_checked:
-                        continue
+                if day_already_checked:
+                    continue
 
-                    data_dict['MACHINE_NR'].append(df[self.const.COL_MACHINE_NR].values[0])
-                    data_dict['BEGIN_DATE'].append(day)
+                data_dict['MACHINE_NR'].append(df[self.const.COL_MACHINE_NR].values[0])
+                data_dict['BEGIN_DATE'].append(day)
 
-                    end_date: datetime.datetime = string_to_date2(day)
-                    while True:
-                        if (end_date + datetime.timedelta(days=1)).strftime('%Y-%m-%d') in missing_days:
-                            end_date += datetime.timedelta(days=1)
-                        else:
-                            break
-                    data_dict['END_DATE'].append(end_date.strftime('%Y-%m-%d'))
+                end_date: datetime.datetime = string_to_date2(day)
+                while True:
+                    if (end_date + datetime.timedelta(days=1)).strftime('%Y-%m-%d') in missing_days:
+                        end_date += datetime.timedelta(days=1)
+                    else:
+                        break
+                data_dict['END_DATE'].append(end_date.strftime('%Y-%m-%d'))
 
-                    prev_day: datetime.datetime = string_to_date2(day) - datetime.timedelta(days=1)
-                    prev_day_str: str = prev_day.strftime('%Y-%m-%d')
-                    if prev_day_str in df[self.const.COL_DATE].unique():
-                        temp_df: pd.DataFrame = df.loc[df[self.const.COL_DATE] == prev_day_str]
-                        data_dict['PREV_STATE'].append(temp_df.tail(1)[self.const.COL_STATUS_CODE].values[0])
+                prev_day: datetime.datetime = string_to_date2(day) - datetime.timedelta(days=1)
+                prev_day_str: str = prev_day.strftime('%Y-%m-%d')
+                if prev_day_str in df[self.const.COL_DATE].unique():
+                    temp_df: pd.DataFrame = df.loc[df[self.const.COL_DATE] == prev_day_str]
+                    data_dict['PREV_STATE'].append(temp_df.tail(1)[self.const.COL_STATUS_CODE].values[0])
 
-                    next_day: datetime.datetime = end_date + datetime.timedelta(days=1)
-                    temp_df: pd.DataFrame = df.loc[df[self.const.COL_DATE] == next_day.strftime('%Y-%m-%d')]
-                    data_dict['NEXT_STATE'].append(temp_df.head(1)[self.const.COL_STATUS_CODE].values[0])
-                print(data_dict)
+                next_day: datetime.datetime = end_date + datetime.timedelta(days=1)
+                temp_df: pd.DataFrame = df.loc[df[self.const.COL_DATE] == next_day.strftime('%Y-%m-%d')]
+                data_dict['NEXT_STATE'].append(temp_df.head(1)[self.const.COL_STATUS_CODE].values[0])
+
         with open(join(self.const.DIR_DATA_GAPS, "data_gaps.csv"), "w+") as out_file:
             out_file.write(pd.DataFrame(data=data_dict).to_csv())
 
