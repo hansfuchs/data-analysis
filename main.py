7a7f993fda2eccf89c23f7b2ece99689ff40d478
hansfuchs <f.hanschristian@gmail.com>
2019-04-30 07:59:55 +0200

    replaced amount of days with total amount of days

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ if __name__ == "__main__":
     )
 
     #csv_generator.prepare_csvs()
-
-    csv_generator.generate_csvs_of_unique_machines(
+    csv_generator.generate_machine_series_csvs(
     # mm/dd/yyyy
         "07/24/2013",
-        14
+        696
     )
-
-    # csv_generator.generate_csvs_from_unique_machines()
+    csv_generator.generate_machine_csvs()
 
     # graph_generator: GraphGenerator = GraphGenerator("07/24/2013", 14)
     # graph_generator.generate_plots()
